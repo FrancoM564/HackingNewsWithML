b4d96bf6e186447bacd4256d296619a2066058dd
Franco Marquez <20191218@aloe.ulima.edu.pe>
2023-04-04 16:36:36 -0500

    Initial Commit

---
+++ hackerNewsRecognition/hackerNewsRecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  hackerNewsRecognitionApp.swift
+//  hackerNewsRecognition
+//
+//  Created by Franco Marquez on 4/04/23.
+//
+
+import SwiftUI
+
+@main
+struct hackerNewsRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
